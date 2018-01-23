c21e368fb9c35619da97702b7d33b07f0f2728ad
Anirudh Sharma <anirudh03sharma@gmail.com>
2018-01-23 15:42:10 +0530

    Update SplashActivity.java

--- app/src/main/java/org/anirudh/redquark/quarkplayer/SplashActivity.java
+++ app/src/main/java/org/anirudh/redquark/quarkplayer/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         /* We do not have setContentView() for this SplashActivity.
         View is displaying from the theme and this way, it is faster than creating a layout. */
 
-        /* Starting UserTypeActivity.java */
+        /* Starting LoginActivity.java */
         startActivity(new Intent(this, LoginActivity.class));
     }
 }
